484ac6295ec123a4a7f55b44148cda0b042f2f13
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-05 21:02:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
+	 const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i] + 1);
+    }
+
+    return result;
   //your code here
 }
+const incrementedArr = incrementArray(arr);
 
 alert(incrementArray(arr));
